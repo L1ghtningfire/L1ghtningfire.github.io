22d5a9f32caa470426e74e2f432a35a5af7260dc
L1ghtningfire <150358286+L1ghtningfire@users.noreply.github.com>
2023-11-09 20:34:18 +0100

    app.js

--- app.cjs
+++ app.cjs
@@ -2,17 +2,36 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment variable for port
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs'); // Add this line
-
-// Set the views directory
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// ... rest of your code ...
+// Serve static files (CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to MongoDB (Replace 'your-mongodb-connection-string' with your MongoDB URL)
+mongoose.connect('your-mongodb-connection-string', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  birthdate: Date,
+  description: String,
+  phone: String,
+  organization: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+// Configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve HTML file with a form to input data and display people from the database
 app.get('/', (req, res) => {
@@ -20,49 +39,17 @@ app.get('/', (req, res) => {
   Person.find({}, (err, people) => {
     if (err) {
       console.error('Database Query Error:', err);
-      res.render('index', { people: [] }); // Pass an empty array in case of an error
+      res.render('index', { people: [] });
     } else {
-      res.render('index', { people: people }); // Pass the found people to the view
+      res.render('index', { people: people });
     }
   });
-});
-
-
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/peopleDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  birthdate: Date, // Change age to birthdate
-  description: String,
-  phone: String,
-  organization: String,
-});
-
-
-
-
-
-const Person = mongoose.model('Person', personSchema);
-
-// Configure body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(__dirname + '/public'));
-
-// Serve HTML file with a form to input data
-app.get('/', (req, res) => {
-  res.render('index', { people: [] }); // Pass an empty array initially
 });
 
 // Handle form submission to store data in MongoDB
 app.post('/add-person', (req, res) => {
   const name = req.body.name;
-  const birthdate = new Date(req.body.birthdate); // Parse the date input
+  const birthdate = new Date(req.body.birthdate);
   const description = req.body.description;
 
   const newPerson = new Person({
@@ -80,80 +67,21 @@ app.post('/add-person', (req, res) => {
   });
 });
 
-
 // Handle search form submission
 app.post('/search', (req, res) => {
   const searchTerm = req.body.searchTerm;
 
   // Perform a database query to find people matching the search term
   Person.find({ name: { $regex: searchTerm, $options: 'i' } }, (err, foundPeople) => {
-      if (err) {
-          console.error('Database Query Error:', err);
-      } else {
-          res.render('index', { people: foundPeople });
-      }
-  });
-});
-
-// Add a route to render the person-info page
-app.get('/person/:id', (req, res) => {
-  const personId = req.params.id;
-
-  // Find the person by their ID in the database
-  Person.findById(personId, (err, foundPerson) => {
     if (err) {
       console.error('Database Query Error:', err);
-    } else if (!foundPerson) {
-      // Handle the case where the person is not found
-      // Render a 404 page or provide an error message
     } else {
-      // Render the person-info template with the found person
-      res.render('person-info', { person: foundPerson });
+      res.render('index', { people: foundPeople });
     }
   });
 });
 
-app.post('/person/edit/:id', (req, res) => {
-  const personId = req.params.id;
-  const updatedDescription = req.body.description;
-  const updatedPhone = req.body.phone;
-  const updatedOrganization = req.body.organization;
-  const updatedBirthdate = new Date(req.body.birthdate); // Parse the date input
-
-  Person.findByIdAndUpdate(
-    personId,
-    {
-      description: updatedDescription,
-      phone: updatedPhone,
-      organization: updatedOrganization,
-      birthdate: updatedBirthdate, // Update the birthdate
-    },
-    (err, updatedPerson) => {
-      if (err) {
-        console.error('Database Update Error:', err);
-      } else {
-        res.redirect(`/person/${personId}`);
-      }
-    }
-  );
-});
-
-
-
-
-// Add this route to handle person deletion
-app.get('/person/:id/delete', (req, res) => {
-  const personId = req.params.id;
-
-  // Find the person by their ID and remove them from the database
-  Person.findByIdAndRemove(personId, (err, removedPerson) => {
-    if (err) {
-      console.error('Database Delete Error:', err);
-    } else {
-      res.redirect('/');
-    }
-  });
-});
+// ... Rest of your routes for editing and deleting
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
